b5725572f396d4604d70d70d917c8f86cb559735
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-06-25 11:29:50 +0300

    create empty branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import {AboutEventComponent} from './steps/about-event/about-event.component';
-import {ContactsComponent} from './steps/contacts/contacts.component';
-import {StepperComponent} from './components/stepper/stepper.component';
-import {HeaderComponent} from './components/header/header.component';
-import {FormEventComponent} from './components/form-event/form-event.component';
-import {SubmitButtonComponent} from './components/submit-button/submit-button.component';
-import {HttpClientModule} from '@angular/common/http';
-import {SummaryInfoComponent} from './components/summary-info/summary-info.component';
-import {FormContactsComponent} from './components/form-contacts/form-contacts.component';
-import {PhoneMaskDirective} from './directives/phone-mask.directive';
-import {RubCurrencyPipe} from './pipes/rub-currency.pipe';
-import {FormDataService} from './services/form-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutEventComponent,
-    ContactsComponent,
-    StepperComponent,
-    HeaderComponent,
-    FormEventComponent,
-    SubmitButtonComponent,
-    SummaryInfoComponent,
-    FormContactsComponent,
-    PhoneMaskDirective,
-    RubCurrencyPipe,
-  ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
-  providers: [FormDataService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/types/eventForm.ts
+++
@@ -1,9 +0,0 @@
-import {FormControl} from '@angular/forms';
-
-export interface EventInfoForm {
-  formEventName: FormControl<string | null>;
-  countGuests: FormControl<number | null>;
-  date: FormControl<Date | null>;
-  additionService: FormControl<string[] | null>;
-  menuWishes: FormControl<string | null>;
-}
