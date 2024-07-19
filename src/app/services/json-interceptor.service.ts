7cf60e86abf5eb8ec1a87bb315cbc28d704b1ed2
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-07-19 10:12:17 +0300

    fix: type in JsonInterceptorService

--- src/app/services/json-interceptor.service.ts
+++ src/app/services/json-interceptor.service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable()
 export class JsonInterceptorService implements HttpInterceptor {
-  public intercept(req: HttpRequest<string>, next: HttpHandler): Observable<HttpEvent<HttpEventType>> {
+  public intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let url: string = req.url;
 
     if (!url.endsWith('.json')) {
       url += '.json';
     }
 
-    const clonedReq: HttpRequest<string> = req.clone({url});
+    const clonedReq: HttpRequest<unknown> = req.clone({url});
 
     return next.handle(clonedReq);
   }
